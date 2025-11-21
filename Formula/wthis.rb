3ce68e0d2576a780da772bddcd20f05ff2991b72
Eltrac <hi@guhub.cn>
2025-11-21 14:37:04 +0800

    chore: bump wthis version v0.1.1

--- Formula/wthis.rb
+++ Formula/wthis.rb
@@ -1,8 +1,8 @@
 class Wthis < Formula
   desc "What the heck is in my Homebrew list?"
   homepage "https://github.com/BigCoke233/wthis"
-  url "https://github.com/BigCoke233/wthis/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "8bd886cdd9ac564441459db6fff3358f69442bf2fb0ea587c496c63ac5442242"
+  url "https://github.com/BigCoke233/wthis/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "MIT"
   head "https://github.com/BigCoke233/wthis.git", branch: "main"
 
@@ -14,6 +14,6 @@ class Wthis < Formula
 
   test do
     output = shell_output("#{bin}/wthis --version 2>&1")
-    assert_match "0.1.0", output
+    assert_match "0.1.1", output
   end
 end
