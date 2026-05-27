193862d281871184394d4f924d59860b0743f20e
eltrac <hi@eltr.ac>
2026-05-27 14:50:32 +0800

    bump version, wthis v0.2.2

--- Formula/wthis.rb
+++ Formula/wthis.rb
@@ -1,8 +1,8 @@
 class Wthis < Formula
   desc "What the heck is in my Homebrew list?"
   homepage "https://codeberg.org/eltrac/wthis"
-  url "https://codeberg.org/eltrac/wthis/archive/v0.2.1.tar.gz"
-  sha256 "871d72186653d3e39ea1b841872c8f450c8908631d36e1e32db6195f8d84b8bc"
+  url "https://codeberg.org/eltrac/wthis/archive/v0.2.2.tar.gz"
+  sha256 "f3e78273d33aecca87528cbdb242ad7c769d5fdc370b07205c7a163fb62a752e"
   license "MIT"
   head "https://codeberg.org/eltrac/wthis.git", branch: "main"
 
@@ -14,6 +14,6 @@ class Wthis < Formula
 
   test do
     output = shell_output("#{bin}/wthis --version 2>&1")
-    assert_match "0.2.1", output
+    assert_match "0.2.2", output
   end
 end
